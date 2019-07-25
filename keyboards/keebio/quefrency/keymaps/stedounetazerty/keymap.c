467ef2be5f703f2193b31b4f2440eaa3f2b0efb9
LaCancellera <lacancellera.yoann@gmail.com>
2019-07-25 07:03:58 +0000

    Add: numlock

--- keyboards/keebio/quefrency/keymaps/stedounetazerty/keymap.c
+++ keyboards/keebio/quefrency/keymaps/stedounetazerty/keymap.c
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_FN1] = LAYOUT_65(
-    KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,       KC_F7,   KC_F8,   KC_F9,    KC_F10,   KC_F11,  KC_F12,  KC_DEL,  KC_DEL, _______,
+    KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,       KC_F7,   KC_F8,   KC_F9,    KC_F10,   KC_F11,  KC_F12,  KC_DEL,  KC_DEL, KC_NLCK,
     _______, KC_BRIU, KC_VOLU, LWIN(KC_L),   KC_END,  DF(_BASE),                _______, KC_P7,    KC_P8,    KC_P9,   _______, _______, _______, _______, _______,
     _______, KC_BRID, KC_VOLD, KC_DOWN, KC_RGHT, DF(_UNSPLIT),                  _______, KC_P4,    KC_P5,    KC_P6,   _______, _______, _______, _______,
     _______, _______, KC_MUTE, _______, _______, _______,                       _______, KC_P1,    KC_P2,    KC_P3,   _______, _______, A(KC_UP), _______,
