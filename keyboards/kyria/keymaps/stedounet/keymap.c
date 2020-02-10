e340433e6a41eae7d17be0e8d9b2323ac0d80dbe
root <root@DESKTOP-MMSO0E1.localdomain>
2020-02-10 22:01:04 +0100

    Finishing kyria

--- keyboards/kyria/keymaps/stedounet/keymap.c
+++ keyboards/kyria/keymaps/stedounet/keymap.c
@@ -16,7 +16,7 @@
 #include QMK_KEYBOARD_H
 
 enum layers {
-    _QWERTY = 0,
+    _AZERTY = 0,
     _LOWER,
     _RAISE,
     _ADJUST
@@ -37,6 +37,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        |      |      |      |      |      |  |      |      |      |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
+    [_AZERTY] = LAYOUT(
+      KC_TAB,	FR_A,   FR_Z, KC_E,   KC_R,   KC_T,                                       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, FR_CIRC,
+      MOD_LCTL, FR_Q,   KC_S, KC_D,   KC_F,   KC_G,                                       KC_H,    KC_J,    KC_K,    KC_L,    FR_M, FR_UGRV,
+      KC_LSFT,  KC_Z,   KC_X, KC_C,   KC_V,   KC_B,KC_LSFT,MO(_ADJUST), KC_LSFT, KC_LSFT, KC_N,    FR_COMM, FR_SCLN, FR_COLN, FR_EXLM, FR_ASTR,
+
+          KC_LGUI, KC_LALT, MO(_LOWER), KC_SPC, MO(_RAISE),             KC_ENT,  KC_BSPC, ALGR(MO(_LOWER)), KC_DEL,  KC_RALT
+    ),
 
 /*
  * Lower Layer: Symbols
@@ -53,6 +60,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        `----------------------------------'  `----------------------------------'
  */
 
+    [_LOWER] = LAYOUT(
+      _______, FR_AMP,  FR_EACU, FR_QUOT, FR_APOS, FR_LPRN,                                    FR_MINS, FR_EGRV, FR_UNDS, FR_CCED, FR_AGRV,FR_RPRN,
+      _______, KC_CIRC, KC_DLR,  KC_LPRN, KC_LBRC, LSFT(KC_LBRC),                                   _______, _______, _______, _______, KC_PERC, FR_EQL,
+      _______, KC_PERC, KC_HASH, KC_RPRN, KC_RBRC, LSFT(KC_RBRC), _______, _______, _______, KC_UP, _______, _______,  KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
+                                 _______, _______, _______,       KC_SCLN, KC_EQL,  KC_RIGHT, KC_DOWN, KC_LEFT, _______
+    ),
 /*
  * Raise Layer: Number keys, media, navigation
  *
@@ -67,24 +80,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        |      |      |      |      |      |  |      |      |      |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
-    [_QWERTY] = LAYOUT(
-      LT(_RAISE, KC_ESC),       KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_PIPE,
-      MT(MOD_LCTL, KC_BSPC),   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-      KC_LSFT,                 KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_LSFT,   KC_LSFT, KC_LSFT, KC_LSFT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
-              KC_LGUI, KC_DEL, MT(MOD_LALT, KC_ENT), LT(_LOWER, KC_SPC), LT(_RAISE, KC_ESC), LT(_LOWER, KC_ENT), LT(_RAISE, KC_SPC), KC_TAB,  KC_BSPC, KC_RALT
-    ),
 
-    [_LOWER] = LAYOUT(
-      _______, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE,                                     _______, _______, _______, _______, _______, KC_BSLS,
-      _______, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,                                      KC_PLUS, KC_MINS, KC_SLSH, KC_ASTR, KC_PERC, KC_QUOT,
-      _______, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD, _______, _______, _______, _______, KC_AMPR, KC_EQL,  KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
-                                 _______, _______, _______, KC_SCLN, KC_EQL,  KC_EQL,  KC_SCLN, _______, _______, _______
-    ),
     [_RAISE] = LAYOUT(
-      _______, KC_1, 	  KC_2,    KC_3,    KC_4,    KC_5,                                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-      _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLU,                                     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
-      _______, _______, _______, _______, KC_MUTE, KC_VOLD, _______, _______, _______, _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______,
-                                 _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+      _______, KC_1, 	KC_BRIU,    KC_3,  KC_VOLU, _______,                                      KC_P7,   KC_P8,   KC_P9,    KC_NLCK, _______, _______,
+      _______, _______, KC_BRID, KC_MPLY, KC_VOLD, _______,                                     KC_P4,   KC_P5,   KC_P6,    _______, _______, _______,
+      _______, _______, _______, _______, KC_MUTE, _______, _______, _______, _______, _______, KC_P1,	 KC_P2,   KC_P3,  KC_MS_R, _______, _______,
+                                 _______, _______, _______, _______, _______, _______, _______, KC_P0,   KC_PDOT, _______
     ),
 /*
  * Adjust Layer: Function keys, RGB
@@ -168,7 +169,7 @@ static void render_status(void) {
     // Host Keyboard Layer Status
     oled_write_P(PSTR("Layer: "), false);
     switch (get_highest_layer(layer_state)) {
-        case _QWERTY:
+        case _AZERTY:
             oled_write_P(PSTR("Default\n"), false);
             break;
         case _LOWER:
