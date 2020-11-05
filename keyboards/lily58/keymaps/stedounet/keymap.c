55c667307912ff528b32034eac4152863a6854f2
LaCancellera <lacancellera.yoann@gmail.com>
2020-11-05 21:56:49 +0000

    Fix: lily algr

--- keyboards/lily58/keymaps/stedounet/keymap.c
+++ keyboards/lily58/keymaps/stedounet/keymap.c
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  FR_A,    FR_Z,    KC_E,    KC_R,    KC_T,                         KC_Y,   KC_U,    KC_I,    KC_O,   KC_P,    FR_CIRC,
         KC_LCTL, FR_Q,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,   KC_J,    KC_K,    KC_L,   FR_M,    FR_UGRV, 
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LGUI,    KC_RBRC, KC_N,   FR_COMM, FR_SCLN, FR_COLN,FR_EXLM, FR_ASTR,
-        	 	     KC_LGUI, KC_LALT, MO(1),   KC_SPC,                KC_ENT,  KC_BSPC, FR_ALGR, FR_LABK
+        	 	     KC_LGUI, KC_LALT, MO(1),   KC_SPC,                KC_ENT,  KC_BSPC, KC_ALGR, FR_LABK
     ),
     [1] = LAYOUT(
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,         KC_F11,
